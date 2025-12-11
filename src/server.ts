f718f67e368fb370a84344ac599c7e94432adc63
Enzo Vasconcelos <enzo.vasconcelos@ccc.ufcg.edu.br>
2025-12-11 11:07:28 -0300

    docs: add diagram class, swagger ui and update readme

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import express from "express";
 import userRoutes from "./routes/userRoutes";
 import postRoutes from "./routes/postRoutes";
 import gameRoutes from "./routes/gameRoutes";
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 
@@ -13,6 +15,7 @@ app.use(express.json());
 app.use("/api", userRoutes);
 app.use("/api", postRoutes);
 app.use("/api", gameRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const PORT = 3000;
 
